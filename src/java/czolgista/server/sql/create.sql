c59145438ad2976b9fc6f5d1c1b2439177d776a0
Maciek Naruszewicz <maciek.naruszewicz@gmail.com>
2012-11-21 19:41:00 +0100

    Update changes so that authentication works

--- src/java/czolgista/server/sql/create.sql
+++ src/java/czolgista/server/sql/create.sql
@@ -16,6 +16,13 @@ create table `highscores` (
     PRIMARY KEY (id)
 );
 
+create table `groups` (
+    id smallint NOT NULL AUTO_INCREMENT,
+    username varchar(128) NOT NULL UNIQUE,
+    groupname varchar(32) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 insert into users values (DEFAULT, 'user_1', 'haslo_1');
 insert into users values (DEFAULT, 'user_2', 'haslo_2');
 insert into users values (DEFAULT, 'user_3', 'haslo_3');
@@ -23,3 +30,7 @@ insert into users values (DEFAULT, 'user_3', 'haslo_3');
 insert into highscores values (DEFAULT, 1, 5);
 insert into highscores values (DEFAULT, 2, 10);
 insert into highscores values (DEFAULT, 3, 15);
+
+insert into groups values (DEFAULT, 'user_1', 'standard_users');
+insert into groups values (DEFAULT, 'user_2', 'standard_users');
+insert into groups values (DEFAULT, 'user_3', 'admins');
